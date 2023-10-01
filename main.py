3ce92cbee4788cac4233693dcca950596818af5d
0KeTeR0 <louis.demeocq@gmail.com>
2023-10-01 22:20:45 +0200

    Ajout du projet et début du code de la fonction main

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+clock = pygame.time.Clock()
+
+
+def main():
+    running = True
+
+    position = set()
+    while running:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
